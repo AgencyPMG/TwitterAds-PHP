4ef0d942ecd4c1fd1ff2b4c6482d403c8c572a13
Blake LaFleur <blake.lafleur@pmg.co>
2016-07-14 18:00:35 -0500

    Testing rate limits

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->addPsr4('PMG\\TwitterAds\\', __DIR__);
 
+//Only load env file if it exists. 
 if (file_exists(__DIR__.'/.env')) {
     $dotenv = new Dotenv\Dotenv(__DIR__);
     $dotenv->load();
